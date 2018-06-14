c8264fdc96b6660f1408d8c0d34f3d6fdc192f85
jeremyfa <contact@jeremyfa.com>
2018-06-14 11:45:22 +0200

    Add original error in SerializationException message

--- spine/support/utils/SerializationException.hx
+++ spine/support/utils/SerializationException.hx
@@ -4,6 +4,14 @@ class SerializationException extends Error {
 
 	public function new (message:String = "", ?originalError:Dynamic) {
 
+		if (originalError != null) {
+			if (message != '') {
+				message += ' ' + originalError;
+			} else {
+				message = '' + originalError;
+			}
+		}
+
 		super (message);
 		name = "SerializationException";
 
